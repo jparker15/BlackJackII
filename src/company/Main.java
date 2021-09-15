1a557d3959f8234fc09ad5e18711c66974de8553
Jeremy <jeremyeparker@yahoo.com>
2021-09-14 20:21:21 -0400

    test cheater deck

--- src/company/Main.java
+++ src/company/Main.java
@@ -3,6 +3,17 @@ package company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        CheaterDeck d = new CheaterDeck();
+//
+//        d.shuffle();
+//        d.printDeck();
+//        System.out.println(d.deal());
+//
+//        System.out.println("NEW \n\n\n");
+//        d.shuffle();
+//        d.printDeck();
+//        System.out.println(d.deal());
+        System.out.println(d.deal());
     }
 }
