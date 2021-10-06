29b46e93b4b45e6a1f8492c2c1b59d3d9b432243
Jermy <jeremyeparker@yahoo.com>
2021-10-05 23:48:27 -0400

    a lot todo

--- src/company/Main.java
+++ src/company/Main.java
@@ -5,15 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         CheaterDeck d = new CheaterDeck();
-//
-//        d.shuffle();
-//        d.printDeck();
-//        System.out.println(d.deal());
-//
-//        System.out.println("NEW \n\n\n");
-//        d.shuffle();
-//        d.printDeck();
-//        System.out.println(d.deal());
-        System.out.println(d.deal());
+
     }
 }
